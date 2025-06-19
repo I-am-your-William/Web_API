045aec9e643045f56870e321c8631040c89e2367
Joe Shen <p20012706@student.newinti.edu.my>
2025-06-20 05:32:52 +0800

    vite config

--- frontend/clerk-react/vite.config.ts
+++ frontend/clerk-react/vite.config.ts
@@ -15,9 +15,8 @@ export default defineConfig(({ mode }) => {
         "@shared": path.resolve(__dirname, "..", "shared"),
         "@assets": path.resolve(__dirname, "..", "attached_assets"),
       },
-    },
-    build: {
-      outDir: path.resolve(__dirname, "..", "dist/public"),
+    },    build: {
+      outDir: "dist",
       emptyOutDir: true,
     },
     server: {
